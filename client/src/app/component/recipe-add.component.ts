fb42bd16d03a8817a2576759ff7469ad29c57c5f
Elvinardy Darwin <elvinardy_darwin@Elvinardys-MacBook-Pro.local>
2022-02-11 15:33:51 +0800

    5th commit

--- client/src/app/component/recipe-add.component.ts
+++ client/src/app/component/recipe-add.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { timeStamp } from 'console';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { RecipelistComponent } from './recipelist.component';
 
@@ -17,22 +16,43 @@ export class RecipeAddComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder, private recipeSvc: RecipeService) { }
+
+  constructor(private fb: FormBuilder, private recipeSvc: RecipeService, private router: Router) { }
 
   ngOnInit(): void {
    /*  this.resetForm(); */
-
-  }
-
-  createAddForm() {
-    this.form = this.fb.group({
-      title: this.fb.control('', [Validators.required, Validators.minLength(3)]),
-      ingredient: this.fb.array('',[Validators.required, Validators.minLength(3)]);
+   this.form = this.fb.group({
+      title: this.fb.control('', [Validators.required,
+        Validators.minLength(3)]),
+      image: this.fb.control('', Validators.required),
+      instruction: this.fb.control('', [Validators.required,
+        Validators.minLength(3)]),
+      ingredient: this.fb.array([new FormControl
+        ('',[Validators.required, Validators.minLength(3)]),
+       ])
     })
   }
 
-  addRecipe() {
-    const add = this.recipeList
+  addUserInput() {
+    const ctrl = new FormControl('',[Validators.required,
+    Validators.minLength(3)]);
+    (<FormArray>this.form.get('ingredients')).push(ctrl);
+  }
+
+  deleteInput(i: number) {
+    (<FormArray>this.form.get('ingredients')).removeAt(i);
+  }
+
+  getFormArray(): FormArray {
+    return this.form.get('ingredients') as FormArray;
+  }
+
+  goBack(): void{
+    this.router.navigate(['/']);
+  }
+
+  submitForm(): void {
+
   }
 
 }
